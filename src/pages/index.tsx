61ed809f4dcde35439167093669e3ff96889453d
Lucas Prazeres <lucascprazeres@outlook.com>
2022-07-11 17:29:37 -0300

    feat: add hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
+import { Logo } from '../components/Logo'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -9,7 +11,34 @@ const Home: NextPage = () => {
         <title>traveler | home</title>
       </Head>
 
-      <h1>Hello, world</h1>
+      <div className={styles.content}>
+        <header>
+          <Logo />
+
+          <span />
+        </header>
+
+        <main>
+          <section className={styles.hero}>
+            <h1>
+              Viva uma grande aventura
+            </h1>
+
+            <p>
+              Descubra locais incríveis para se <br />
+              visitar em cidades maravilhosas do <br /> Pará.
+            </p>
+
+            <Link href="#">
+              <a>
+                Descobrir todos os lugares
+              </a>
+            </Link>
+          </section>
+
+          <section className={styles.places}></section>
+        </main>
+      </div>
     </div>
   )
 }
